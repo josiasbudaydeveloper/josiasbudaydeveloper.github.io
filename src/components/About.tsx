83c9e4ce30329d6660864b81531915430de88608
Josias Buday Dias <josiasbudaydeveloper@gmail.com>
2024-01-25 21:10:58 -0300

    Updating the About section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,51 +1,52 @@
-import { useEffect, useState } from "react";
-
 export default function About() {
-  const BIRTHDATE: Date = new Date('1999-10-14');
-
-  const [currentYears, setCurrentYears] = useState(0);
-
-  function calculateAge(birthDate: Date) : number {
-    const now = new Date();
-    let yearDiff = now.getFullYear() - birthDate.getFullYear();
-    const monthDiff = now.getMonth() - birthDate.getMonth();
-    const dayDiff = now.getDate() - birthDate.getDate();
-
-    // Adjust age when birth date is later in the year
-    if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
-      yearDiff--;
-    }
-
-    return yearDiff;
-  }
-
-
-  useEffect(() => {
-    setCurrentYears(calculateAge(BIRTHDATE));
-  }, []);
-  
   return (
   <section id="about" className="paddingX paddingY">
     <header><h1 className="emphasize main-header">About me</h1></header>
     <p className="paragraph">
-      Hi, my name's Josias Buday Dias. I'm {currentYears} years old and I'm from Brazil. 
-      I have a degree in Systems Analysis and Development from Universidade Estácio de Sá, a 
-      technological higher education course that is similar to an associate's degree <br /><br /> 
+      Full Stack React/Next Developer with TypeScript.<br /><br />
 
-      Currenly, I'm studying and doing the Next.js 14's Official Tutorial, a beautiful 
-      and responsive SPA (Single Page Application) Dashboard, 
-      totally based on the Next.js's App Router feature and all the main features of a 
-      complete application: <br /><br />
-        - Authentication <br />
-        - Serverless <br />
-        - PostGreSQL on Vercel <br />
-        - Pagination <br />
-      <br /><br />
-      I consider myself at the B1 English Speaking Level, but I'm always improving my English skills, through: <br /><br />
-        - Asking ChatGPT (mainly with Bing AI) <br />
-        - Reading Documentation and Articles in English <br />
-        - Watching video tutorials in English on YouTube. <br />
-      <br /><br />
+      In summary, I'm capable of develop: <br /><br />
+
+      Complete Full-Stack Systems with Next.js 14's App Router:
+      <ul>
+        <li>
+          Many Pages with a beautiful Responsive Web Design that are 
+          Single Page Applications (SPA) and counts with a good SEO
+        </li>
+        <li>Searching bar feature based on search params</li>
+        <li>Pagination</li>
+        <li>Dark Mode with Tailwind.css</li>
+        <li>Authentication with Auth.js and JWT tokens stored on the browser's cookies</li>
+        <li>Database connection that could be pretty much any relational database and MongoDB</li>
+        <li>Hosting it on Vercel or on more advanced cloud providers, in case of necessity</li>
+      </ul>
+
+      Advanced Back-End Systems with Express.js:
+      <ul>
+        <li>RestFul API's with JWT authentication</li>
+        <li>Automated Tests with Jest</li>
+      </ul>
+
+      API's:
+      <ul>
+        <li>
+            I have some experience of working with the OpenAI's API 
+            using the ChatGPT's GPT-3.5-PLUS model
+          </li>
+          <li>
+            I also have some experience of studying how does the Zabbix API works 
+            (based on JSON information sent with the POST HTTP Method), 
+            also known as JSON RPC (Remote Procedure Call).
+          </li>
+      </ul>
+
+      I consider myself at the B1 English Speaking Level, but I'm always improving my English skills, 
+      through:
+      <ul>
+        <li> Asking ChatGPT (mainly with Bing AI) </li>
+        <li> Reading Documentation and Articles in English </li>
+        <li> Watching video tutorials in English on YouTube </li>
+      </ul>
       
     </p>
   </section>
