232c6d1571f4f9302881651a4fd221bc454d5135
Josias Buday Dias <josiasbudaydeveloper@gmail.com>
2024-02-08 18:32:12 -0300

    Adding the "Built with Vite.js"

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -1,13 +1,32 @@
-import Contacts from "./Contacts"
+import Contacts from "./Contacts";
+import Logos from '../assets/technology-logos';
 
 export default function Aside() { return (
   <aside>
     <header><h1>Josias Buday Dias</h1></header>
     <hr />
-    <img src="https://avatars.githubusercontent.com/u/105136787?s=400&u=6af6f17c26fcbef8bf960cdc7257d67dc3c8b4e1&v=4" />
+    <img
+      id="profile-picture"
+      src="https://avatars.githubusercontent.com/u/105136787?s=400&u=6af6f17c26fcbef8bf960cdc7257d67dc3c8b4e1&v=4"
+    />
     <h2>
       A <span className="emphasize">Full-Stack JS</span> Developer
     </h2>
     <Contacts />
+
+    <section style={{
+      display: "flex",
+      alignItems: "center",
+      paddingLeft: "20px",
+      color: "white"
+    }}>
+      Built with Vite.js
+      <img style={{
+        paddingLeft: "5px",
+        width: "40px"
+      }}
+        src={Logos.Vite} 
+      />
+    </section>
   </ aside>
 )}
