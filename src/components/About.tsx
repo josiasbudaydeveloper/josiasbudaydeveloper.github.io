763cba1f010a2241bb81546daf8c7555dfc5a7e3
Josias Buday Dias <josiasbudaydeveloper@gmail.com>
2024-04-04 19:37:31 -0300

    Improving the styles

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,52 +2,51 @@ export default function About() {
   return (
   <section id="about" className="paddingX paddingY">
     <header><h1 className="emphasize main-header">About me</h1></header>
-    <p className="paragraph">
-      Full Stack React/Next Developer with TypeScript.<br /><br />
-
-      In summary, I'm capable of develop: <br /><br />
-
-      Complete Full-Stack Systems with Next.js 14's App Router:
+    <div>
+      <h2>Full Stack JS/TS Developer with Next/Express.</h2>
+      <p>
+        I am a Full Stack developer with JavaScript and TypeScript, specialized in 
+        developing Single Page Applications with React.js and Next.js. I am also able 
+        to develop RestFul APIs with the Express.js framework. Main skills:
+      </p>
+      
+      <h3>Technologies:</h3>
       <ul>
-        <li>
-          Many Pages with a beautiful Responsive Web Design that are 
-          Single Page Applications (SPA) and counts with a good SEO
-        </li>
-        <li>Searching bar feature based on search params</li>
-        <li>Pagination</li>
-        <li>Dark Mode with Tailwind.css</li>
-        <li>Authentication with Auth.js and JWT tokens stored on the browser's cookies</li>
-        <li>Database connection that could be pretty much any relational database and MongoDB</li>
-        <li>Hosting it on Vercel or on more advanced cloud providers, in case of necessity</li>
+        <li>HTML5,</li>
+        <li>CSS3,</li>
+        <li>JavaScript,</li>
+        <li>TypeScript,</li>
+        <li>Bootstrap 5,</li>
+        <li>React-Bootstrap 5,</li>
+        <li>Tailwind.css,</li>
+        <li>Node.js,</li>
+        <li>React.js,</li>
+        <li>Next.js 14,</li>
+        <li>Auth.js 4,</li>
+        <li>Express.js 4,</li>
+        <li>JWT,</li>
+        <li>Jest,</li>
+        <li>MongoDB,</li>
+        <li>SQL,</li>
+        <li>Git</li>
+        <li>GitHub</li>
       </ul>
 
-      Advanced Back-End Systems with Express.js:
+      <h3>Generative Artificial Intelligences:</h3>
+      ChatGPT and Microsoft Copilot (which has speech and image recognition, as 
+      well as image generation via prompt) <br /><br />
+      
+      <h3>Learning:</h3>
       <ul>
-        <li>RestFul API's with JWT authentication</li>
-        <li>Automated Tests with Jest</li>
-      </ul>
-
-      API's:
-      <ul>
-        <li>
-            I have some experience of working with the OpenAI's API 
-            using the ChatGPT's GPT-3.5-PLUS model
-          </li>
-          <li>
-            I also have some experience of studying how does the Zabbix API works 
-            (based on JSON information sent with the POST HTTP Method), 
-            also known as JSON RPC (Remote Procedure Call).
-          </li>
-      </ul>
-
-      I consider myself at the B1 English Speaking Level, but I'm always improving my English skills, 
-      through:
-      <ul>
-        <li> Asking ChatGPT (mainly with Bing AI) </li>
-        <li> Reading Documentation and Articles in English </li>
-        <li> Watching video tutorials in English on YouTube </li>
+        <li>Cross Platform development with React Native (Expo)</li>
+        <li>PayloadJS for Content Management and its integration with the Stripe payment gateway</li>
       </ul>
       
-    </p>
+      <h3>Languages:</h3>
+      <ul>
+        <li>English - Intermediate (ready for writting and reading)</li>
+        <li>Portuguese - Native</li>
+      </ul>
+    </div>
   </section>
 )}
--- src/components/HireMe.tsx
+++ src/components/HireMe.tsx
@@ -1,15 +1,13 @@
 export default function HireMe() { return (
   <section id="hire-me" className="paddingX paddingY">
-    <p className="paragraph">
-      I'm currently: <span className="emphasize">looking</span> for a job! <br />
-      You can see my CV at: 
-      <a 
-        href="https://docs.google.com/document/d/1EGWjHB5KhHXdI9thR4rtA4H-hRUB8J4BwbeNhXSjsO8/edit?usp=sharing" 
-        target="_blank" className="main-anchor emphasize"
-      >
-      {" "}My Full-Stack JS Developer CV
-      </a> <br />
-      If I didn't match your requirements, please consider my for your future applications!
-    </p>
+    <span style={{fontWeight: 'bold'}}>I'm currently:</span> <span className="emphasize">looking</span> for a job! <br />
+    <span style={{fontWeight: 'bold'}}>You can see my CV at:</span>
+    <a 
+      href="https://docs.google.com/document/d/1EGWjHB5KhHXdI9thR4rtA4H-hRUB8J4BwbeNhXSjsO8/edit?usp=sharing" 
+      target="_blank" className="main-anchor emphasize"
+    >
+    {" "}My Full-Stack JS Developer CV
+    </a> <br /><br />
+    If I didn't match your requirements, please consider my for your future applications!
   </section>
 )}
