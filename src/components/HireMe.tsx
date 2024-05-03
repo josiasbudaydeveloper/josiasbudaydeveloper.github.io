e1b67c5c97263adc623ea7e8d0850505b6b58ffe
Josias Buday Dias <josiasbudaydeveloper@gmail.com>
2024-05-03 03:30:25 -0300

    Hiding my CV link

--- src/components/HireMe.tsx
+++ src/components/HireMe.tsx
@@ -1,13 +1,14 @@
 export default function HireMe() { return (
   <section id="hire-me" style={{padding: '40px 40px 0 40px'}}>
     <span style={{fontWeight: 'bold'}}>I'm currently:</span> <span className="emphasize">looking</span> for a job! <br />
-    <span style={{fontWeight: 'bold'}}>You can see my CV at:</span>
+    {/* <span style={{fontWeight: 'bold'}}>You can see my CV at:</span>
     <a 
       href="https://docs.google.com/document/d/1EGWjHB5KhHXdI9thR4rtA4H-hRUB8J4BwbeNhXSjsO8/edit?usp=sharing" 
       target="_blank" className="main-anchor emphasize"
     >
     {" "}My Full-Stack JS Developer CV
-    </a> <br /><br />
+    </a>*/}
+    <br /><br /> 
     If I didn't match your requirements, please consider to send me an email for helping me <br />
     and please consider me for your future applications!
   </section>
